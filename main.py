b907e2c1e78cfe27c358ff46c5d225b192bcab52
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-27 20:16:01 +0000

    Data type hints

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 # height: float
 # is_human: bool
 
+#Declaring the age as an int will give users data type hints if they use the wrong type
+#Arrows shows us the expecting return value of the function has to be a boolean
 
-def police_check(age: int) -> bool:
+def police_check(age: int) -> bool: 
     if age > 18:
         can_drive = True
     else:
